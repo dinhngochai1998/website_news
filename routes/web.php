c4802aa9d0420c66facfa7d092d321c31e31529e
haidn <haidn@toprate.io>
2022-12-28 00:15:36 +0700

    add file

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::prefix('users')->group(function () {
-    Route::get('/logout', 'App\Http\Controllers\UserController@index')->name('user.index');
 
-});
+    Route::get('/users', 'App\Http\Controllers\UserController@index')->name('user.index');
+
